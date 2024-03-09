ca573c609bdd5966ebf523ddc47ba5cb03b08849
kishan <kishan.dubariya@phenompeople.com>
2024-03-09 15:59:39 +0530

    added extraCheese and extraToppings for DeluxePizza

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -5,24 +5,26 @@ public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         this.setPrice(400);
+        this.extraCheese = true;
+        this.extraToppings = true;
     }
 
     public String getBill() {
         String bill = "Base Price Of The Pizza: 400\n";
         int price = 400;
-        if(extraCheese){
+        if (extraCheese) {
             bill += "Extra Cheese Added: 80\n";
-            price+=80;
+            price += 80;
         }
-        if(extraToppings){
+        if (extraToppings) {
             bill += "Extra Toppings Added: 120\n";
-            price+=70;
+            price += 70;
         }
-        if(takeAway){
-            bill+= "Paperbag Added: 20\n";
-            price+=20;
-        } 
-        bill+= "Total Price:" + price;
+        if (takeAway) {
+            bill += "Paperbag Added: 20\n";
+            price += 20;
+        }
+        bill += "Total Price:" + price;
         return bill;
     }
 }
